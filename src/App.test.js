2e908e53129cff203858907040b6dc53487682ae
fandok <fandika.okdiba@gmail.com>
2023-05-29 22:40:39 +0700

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = screen.getByText(
+    "Sewa & Rental Mobil Terbaik di kawasan Medan"
+  );
+  expect(titleText).toBeInTheDocument();
 });
